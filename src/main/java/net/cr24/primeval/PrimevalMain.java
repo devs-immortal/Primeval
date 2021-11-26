4a0d9604ebed7ac8c095cd89b6c5e87acdd6585c
24Chrome <m.batt1@outlook.com>
2021-11-25 21:38:03 -0600

    updated tags

--- src/main/java/net/cr24/primeval/PrimevalMain.java
+++ src/main/java/net/cr24/primeval/PrimevalMain.java
@@ -7,9 +7,6 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class PrimevalMain implements ModInitializer, ClientModInitializer {
