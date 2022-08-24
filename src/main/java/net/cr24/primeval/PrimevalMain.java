61c9b4fa21eed1b3537365eb614b8e648c0f5d5e
rafy <lplata1903@gmail.com>
2022-08-24 15:19:36 -0400

    Set Up Fabric Data Generator Entrypoint + Added "Minecraft Datagen" run configuration. Generated data is directed to the "src/main/generated" directory. + Added fabric-datagen to fabric.mod.json entrypoints + Implemented DataGeneratorEntrypoint to PrimevalMain

--- src/main/java/net/cr24/primeval/PrimevalMain.java
+++ src/main/java/net/cr24/primeval/PrimevalMain.java
@@ -1,20 +1,21 @@
 package net.cr24.primeval;
 
 import net.cr24.primeval.block.PrimevalBlocks;
+import net.cr24.primeval.data.*;
 import net.cr24.primeval.entity.PrimevalVillagerTrades;
 import net.cr24.primeval.fluid.PrimevalFluids;
-import net.cr24.primeval.item.PrimevalFoodComponents;
 import net.cr24.primeval.item.PrimevalItems;
 import net.cr24.primeval.recipe.PrimevalRecipes;
-import net.cr24.primeval.recipe.rei.PrimevalREIIntegration;
 import net.cr24.primeval.world.PrimevalWorld;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.util.Identifier;
 
-public class PrimevalMain implements ModInitializer, ClientModInitializer {
+public class PrimevalMain implements ModInitializer, ClientModInitializer, DataGeneratorEntrypoint {
 
     private static final String MODID = "primeval";
 
