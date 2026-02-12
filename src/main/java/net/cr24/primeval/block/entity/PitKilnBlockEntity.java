fb169e0e9882a34a920fd4f0d0801178f10a15c5
Maxx <53229958+MBatt1@users.noreply.github.com>
2026-02-11 22:44:57 -0600

    lower pit firing times

--- src/main/java/net/cr24/primeval/block/entity/PitKilnBlockEntity.java
+++ src/main/java/net/cr24/primeval/block/entity/PitKilnBlockEntity.java
@@ -29,8 +29,8 @@ import java.util.Stack;
 
 public class PitKilnBlockEntity extends BlockEntity implements Clearable {
 
-    public static final int[] FIRING_TIMES = new int[] {-1, 9600, 13200, 16800, 19200};
-    //public static final int[] FIRING_TIMES = new int[] {-1, 100, 100, 100, 100};
+    public static final int[] FIRING_TIMES = new int[] {-1, 6200, 8800, 11200, 12800};
+    //public static final int[] FIRING_TIMES = new int[] {-1, 20, 20, 20, 20};
 
     private Stack<ItemStack> logs;
     private ItemStack[] inventory;
